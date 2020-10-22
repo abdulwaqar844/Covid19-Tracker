a90020975336ffc30104705d5c573cf654f83e18
Waqar <abdulwaqar844@gmail.com>
2020-10-22 10:31:44 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from "./Components/NavBar";
+import InfoBar from "./Components/InfoPanel"
+
+function App() {
+  return (
+    <div >
+      <AppBar/>
+      <InfoBar/>
+    </div>
+  );
+}
+
+export default App;
